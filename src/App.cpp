4aa44f29a585a6c5fa8633f5f09791a0008fe23f
ilhooq <contact@sphilip.com>
2017-10-24 13:40:46 +0200

    Fix App name

--- src/App.cpp
+++ src/App.cpp
@@ -6,15 +6,22 @@
 #include "App.h"
 
 #include "wx/wx.h"
+#include "wx/msgout.h"
 #include "wx/xrc/xmlres.h"
 #include "wx/stdpaths.h"
 #include "MainFrame.h"
 
 bool App::OnInit()
 {
+  // In order to fix APPNAME to wxcid in WX_${APPNAME}_DATA_DIR env var.
+  SetAppName("wxscid");
+
   // Make sure to call this first
   wxInitAllImageHandlers();
   wxXmlResource::Get()->InitAllHandlers();
+
+  wxMessageOutputStderr err;
+  err.Printf(wxT("Data dir: %s.\n"), getDataDir());
 
   // Load all of the XRC files that will be used.
   if (!wxXmlResource::Get()->LoadAllFiles(getDataDir() + "/xrc")) {
