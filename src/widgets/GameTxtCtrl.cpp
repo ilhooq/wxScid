c05a2cd59b53af738820294611bf2aadb8f25bd4
ilhooq <contact@sphilip.com>
2017-11-02 23:37:36 +0100

    Improve declaration of GameTxtCtrl

--- src/widgets/GameTxtCtrl.cpp
+++ src/widgets/GameTxtCtrl.cpp
@@ -11,7 +11,7 @@ BEGIN_EVENT_TABLE(GameTxtCtrl, wxRichTextCtrl)
 END_EVENT_TABLE()
 
 GameTxtCtrl::GameTxtCtrl(wxWindow* parent, wxWindowID id, const wxPoint &pos, const wxSize &size)
-: wxRichTextCtrl( parent, id, "", pos, size,  wxNO_BORDER+wxWANTS_CHARS+wxRE_MULTILINE+wxRE_READONLY)
+: wxRichTextCtrl(parent, id, "", pos, size, wxBORDER_NONE | wxWANTS_CHARS | wxRE_MULTILINE | wxRE_READONLY)
 {
 
 }
